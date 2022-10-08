42cea3669ce07e8d057ec25ba47c6879ce455ca7
hexaque <37326156+hexaque@users.noreply.github.com>
2022-10-08 18:54:57 +0300

    first CollectionView Comp
    new album collection view comp

---
+++ YoutubeExplorePageDemo/CellOne.swift
@@ -0,0 +1,15 @@
+//
+//  CellOne.swift
+//  YoutubeExplorePageDemo
+//
+//  Created by Que on 8.10.2022.
+//
+
+import UIKit
+
+class CellOne: UICollectionViewCell {
+    
+    @IBOutlet weak var labelArtistName: UILabel!
+    @IBOutlet weak var labelSongName: UILabel!
+    @IBOutlet weak var photo: UIImageView!
+}
--- YoutubeExplorePageDemo/ViewController.swift
+++ YoutubeExplorePageDemo/ViewController.swift
@@ -9,19 +9,84 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var collectionViewOne: UICollectionView!
     @IBOutlet weak var moreButton: UIButton!
+    var collection1List = [NewAlbumSongs1]()
     override func viewDidLoad() {
+        collectionViewOne.delegate = self
+        collectionViewOne.dataSource = self
+        
+        dataPrepare()
+        topMoodTasarim()
         super.viewDidLoad()
         
         tabBarController?.selectedIndex = 1
         moreButton.layer.cornerRadius = 15
         moreButton.layer.borderColor = UIColor.white.cgColor
-          moreButton.layer.borderWidth = 0.4
+        moreButton.layer.borderWidth = 0.4
         
         
         
         
     }
+    
+    
+    
+    
+    
+    
+    func dataPrepare(){
+        let  s1 = NewAlbumSongs1(songName: "Dünyadan uzak", artistName: "Mor ve ötesi", photoName: "album1")
+        let  s2 = NewAlbumSongs1(songName: "Cambaz", artistName: "Mor ve ötesi", photoName: "album2")
+        let  s3 = NewAlbumSongs1(songName: " Nothing else matters", artistName: "Metallica", photoName: "album3")
+        let  s4 = NewAlbumSongs1(songName: "Bir kadın çizeceksin", artistName: "Manga", photoName: "album4")
+        let  s5 = NewAlbumSongs1(songName: "Kuzu kuzu", artistName: "Tarkan", photoName: "album5")
+        let  s6 = NewAlbumSongs1(songName: "Master of puppets", artistName: "Metallica", photoName: "album3")
+        collection1List.append(s1)
+        collection1List.append(s2)
+        collection1List.append(s3)
+        collection1List.append(s4)
+        collection1List.append(s5)
+        collection1List.append(s6)
+    }
+}
+
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+       return collection1List.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let song = collection1List[indexPath.row]
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CellOne", for: indexPath) as! CellOne
+        cell.labelSongName.text = song.songName!
+        cell.labelArtistName.text = song.artistName!
+        cell.photo.image = UIImage(named: song.photoName!)
+            
+                
+        return cell
+        
+        
+    
+            
+    }
+    func topMoodTasarim(){
+        let tasarim = UICollectionViewFlowLayout()
+         tasarim.scrollDirection = .horizontal
+         tasarim.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+         tasarim.minimumInteritemSpacing = 0 // yatay
+         tasarim.minimumLineSpacing = 10 // dikey
+        
+         let hucreGenisligi = (collectionViewOne.bounds.width-40)/2
+
+         let hucreUzunlugu = collectionViewOne.bounds.height
+        
+         tasarim.itemSize = CGSize(width: hucreGenisligi, height: hucreUzunlugu)
+         collectionViewOne.collectionViewLayout = tasarim
+        
+    }
+    
     
     
 }
