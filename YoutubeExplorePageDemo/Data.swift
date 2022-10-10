ec04754065361e30ba3433eb5d5b1768c39ece51
hexaque <37326156+hexaque@users.noreply.github.com>
2022-10-10 15:57:00 +0300

    que

--- YoutubeExplorePageDemo/Data.swift
+++ YoutubeExplorePageDemo/Data.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+
 struct NewAlbumSongs1{
    
     
@@ -52,4 +53,77 @@ struct MoodAndGenres{
         self.colorB = colorB
     }
 }
+
+
+extension ViewController {
+    
+    func dataPrepare(){
+        let  s1 = NewAlbumSongs1(songName: "Dünyadan uzak", artistName: "Mor ve ötesi", photoName: "album1")
+        let  s2 = NewAlbumSongs1(songName: "Cambaz", artistName: "Mor ve ötesi", photoName: "album2")
+        let  s3 = NewAlbumSongs1(songName: " Nothing else matters", artistName: "Metallica", photoName: "album3")
+        let  s4 = NewAlbumSongs1(songName: "Bir kadın çizeceksin", artistName: "Manga", photoName: "album4")
+        let  s5 = NewAlbumSongs1(songName: "Kuzu kuzu", artistName: "Tarkan", photoName: "album5")
+        let  s6 = NewAlbumSongs1(songName: "Master of puppets", artistName: "Metallica", photoName: "album3")
+        
+        
+        
+        let  ts1 = TopSongs2(songName: "Dünyadan uzak", artistName: "Mor ve ötesi", photoName: "album1",upOrDownImage: "upIcon")
+        let  ts2 = TopSongs2(songName: "Cambaz", artistName: "Mor ve ötesi", photoName: "album2",upOrDownImage: "dotIcon")
+        let  ts3 = TopSongs2(songName: " Nothing else matters", artistName: "Metallica", photoName: "album3",upOrDownImage: "downIcon")
+        let  ts4 = TopSongs2(songName: "Bir kadın çizeceksin", artistName: "Manga", photoName: "album4",upOrDownImage: "downIcon")
+        let  ts5 = TopSongs2(songName: "Kuzu kuzu", artistName: "Tarkan", photoName: "album5",upOrDownImage: "upIcon")
+        let  ts6 = TopSongs2(songName: "Master of puppets", artistName: "Metallica", photoName: "album3",upOrDownImage: "upIcon")
+        let  ts7 = TopSongs2(songName: "Dünyadan uzak", artistName: "Mor ve ötesi", photoName: "album1",upOrDownImage: "upIcon")
+        let  ts8 = TopSongs2(songName: "Cambaz", artistName: "Mor ve ötesi", photoName: "album2",upOrDownImage: "dotIcon")
+        let  ts9 = TopSongs2(songName: " Nothing else matters", artistName: "Metallica", photoName: "album3",upOrDownImage: "downIcon")
+        let  ts10 = TopSongs2(songName: "Bir kadın çizeceksin", artistName: "Manga", photoName: "album4",upOrDownImage: "downIcon")
+        let  ts11 = TopSongs2(songName: "Kuzu kuzu", artistName: "Tarkan", photoName: "album5",upOrDownImage: "upIcon")
+        let  ts12 = TopSongs2(songName: "Master of puppets", artistName: "Metallica", photoName: "album3",upOrDownImage: "upIcon")
+        
+        
+        let m1 = MoodAndGenres(moodName: "Energy", colorR: 183, colorG: 137, colorB: 248)
+        let m2 = MoodAndGenres(moodName: "Energy1", colorR: 140, colorG: 150, colorB: 160)
+        let m3 = MoodAndGenres(moodName: "Energy1", colorR: 195, colorG: 40, colorB: 28)
+        let m4 = MoodAndGenres(moodName: "Energy", colorR: 252, colorG: 236, colorB: 154)
+        let m5 = MoodAndGenres(moodName: "Energy1", colorR: 40, colorG: 50, colorB: 160)
+        let m6 = MoodAndGenres(moodName: "Energy1", colorR: 190, colorG: 120, colorB: 90)
+        let m7 = MoodAndGenres(moodName: "Energy", colorR: 10, colorG: 20, colorB: 220)
+        let m8 = MoodAndGenres(moodName: "Energy1", colorR: 200, colorG: 5, colorB: 60)
+        let m9 = MoodAndGenres(moodName: "Energy1", colorR: 30, colorG: 220, colorB: 254)
+       
+        collection1List.append(s1)
+        collection1List.append(s2)
+        collection1List.append(s3)
+        collection1List.append(s4)
+        collection1List.append(s5)
+        collection1List.append(s6)
+        
+        
+        collection2List.append(ts1)
+        collection2List.append(ts2)
+        collection2List.append(ts3)
+        collection2List.append(ts4)
+        collection2List.append(ts5)
+        collection2List.append(ts6)
+        collection2List.append(ts7)
+        collection2List.append(ts8)
+        collection2List.append(ts9)
+        collection2List.append(ts10)
+        collection2List.append(ts11)
+        collection2List.append(ts12)
+        
+        
+        collection3List.append(m1)
+        collection3List.append(m2)
+        collection3List.append(m3)
+        collection3List.append(m4)
+        collection3List.append(m5)
+        collection3List.append(m6)
+        collection3List.append(m7)
+        collection3List.append(m8)
+        collection3List.append(m9)
+        
+    }
+}
+
 
